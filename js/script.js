03da66ef450abb78ee87ce913c7df5462c653c3e
Alex Melnikov <alexmlnkv@gmail.com>
2022-01-02 19:58:26 +0300

    desktop version done

---
+++ js/script.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', function() {
+  try {
+    $('.moments__photos').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      closeOnContentClick: false,
+      closeBtnInside: false,
+      mainClass: 'mfp-with-zoom mfp-img-mobile',
+      image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+          return item.el.attr('title') ;
+        }
+      },
+      gallery: {
+        enabled: true
+      },
+      zoom: {
+        enabled: true,
+        duration: 300, // don't foget to change the duration also in CSS
+        opener: function(element) {
+          return element.find('img');
+        }
+      }
+
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
