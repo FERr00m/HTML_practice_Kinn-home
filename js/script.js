789009a1f02389223010d69e5f3f67fd398037ba
Alex Melnikov <alexmlnkv@gmail.com>
2022-01-03 20:57:41 +0300

    adaptive done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
   try {
-    $('.moments__photos').magnificPopup({
+    $('.jq-photos').magnificPopup({
       delegate: 'a',
       type: 'image',
       closeOnContentClick: false,
@@ -24,6 +24,22 @@ window.addEventListener('DOMContentLoaded', function() {
       }
 
     });
+
+    const burgerMenu = () => {
+      const menu = document.querySelector('.js-mobile-nav');
+
+      document.addEventListener('click', (e) => {
+          if (e.target.closest('.js-burger-btn')) {
+              menu.classList.add('opened');
+          } else if (e.target === menu) {
+              return;
+          } else {
+              menu.classList.remove('opened');
+          }
+      });
+    }
+    burgerMenu();
+
   } catch (error) {
     console.error(error);
   }
